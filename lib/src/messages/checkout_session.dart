0b66fb431dfbf75fc438d2672f687a97e070e2fa
Milksheyke <amilksheyke@gmail.com>
2024-09-02 17:08:04 -0400

    Expand CheckoutSession class

--- lib/src/messages/checkout_session.dart
+++ lib/src/messages/checkout_session.dart
@@ -42,24 +42,92 @@ enum PaymentMethodType {
   wechat_pay,
 }
 
-/// https://stripe.com/docs/api/checkout/sessions/object
 @JsonSerializable()
 class CheckoutSession extends Message {
-  /// Doesn't work with enum because there is a dot in it.
-  final String object;
-  final String id;
+  final String? id;
+  final String? object;
+  final int? amountSubtotal;
+  final int? amountTotal;
+  final AutomaticTax? automaticTax;
+  final String? billingAddressCollection;
+  final String? cancelUrl;
   final String? clientReferenceId;
+  final int created;
+  final String currency;
+  final List<dynamic>? customFields;
+  final CustomText? customText;
   final String? customer;
+  final String? customerCreation;
+  final dynamic? customerDetails;
+  final String? customerEmail;
+  final int expiresAt;
+  final String? invoice;
+  final InvoiceCreation? invoiceCreation;
+  final bool livemode;
+  final String? locale;
+  final Map<String, String>? metadata;
+  final String? mode;
   final String? paymentIntent;
-  final List<PaymentMethodType> paymentMethodTypes;
+  final String? paymentLink;
+  final String? paymentMethodCollection;
+  final dynamic? paymentMethodOptions;
+  final List<PaymentMethodType>? paymentMethodTypes;
+  final String? paymentStatus;
+  final PhoneNumberCollection? phoneNumberCollection;
+  final String? recoveredFrom;
+  final String? setupIntent;
+  final String? shippingAddressCollection;
+  final dynamic shippingRate;
+  final List<dynamic>? shippingOptions;
+  final String? status;
+  final String? submitType;
+  final String? subscription;
+  final String? successUrl;
+  final TotalDetails totalDetails;
+  final String? url;
 
   CheckoutSession({
-    required this.object,
     required this.id,
-    required this.paymentMethodTypes,
+    required this.object,
+    this.amountSubtotal,
+    this.amountTotal,
+    this.automaticTax,
+    this.billingAddressCollection,
+    this.cancelUrl,
     this.clientReferenceId,
+    required this.created,
+    required this.currency,
+    this.customFields,
+    this.customText,
     this.customer,
+    this.customerCreation,
+    this.customerDetails,
+    this.customerEmail,
+    required this.expiresAt,
+    this.invoice,
+    this.invoiceCreation,
+    required this.livemode,
+    this.locale,
+    this.metadata,
+    this.mode,
     this.paymentIntent,
+    this.paymentLink,
+    this.paymentMethodCollection,
+    this.paymentMethodOptions,
+    this.paymentMethodTypes,
+    required this.paymentStatus,
+    this.phoneNumberCollection,
+    this.recoveredFrom,
+    this.setupIntent,
+    this.shippingAddressCollection,
+    this.shippingRate,
+    this.shippingOptions,
+    required this.status,
+    this.submitType,
+    this.subscription,
+    required this.successUrl,
+    required this.totalDetails,
+    required this.url,
   });
 
   factory CheckoutSession.fromJson(Map<String, dynamic> json) =>
@@ -67,4 +135,112 @@ class CheckoutSession extends Message {
 
   @override
   Map<String, dynamic> toJson() => _$CheckoutSessionToJson(this);
+}
+
+@JsonSerializable()
+class AutomaticTax {
+  final bool enabled;
+  final dynamic liability;
+  final dynamic status;
+
+  AutomaticTax({
+    required this.enabled,
+    this.liability,
+    this.status,
+  });
+
+  factory AutomaticTax.fromJson(Map<String, dynamic> json) =>
+      _$AutomaticTaxFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AutomaticTaxToJson(this);
+}
+
+@JsonSerializable()
+class CustomText {
+  final dynamic shippingAddress;
+  final dynamic submit;
+
+  CustomText({
+    this.shippingAddress,
+    this.submit,
+  });
+
+  factory CustomText.fromJson(Map<String, dynamic> json) =>
+      _$CustomTextFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CustomTextToJson(this);
+}
+
+@JsonSerializable()
+class InvoiceCreation {
+  final bool enabled;
+  final InvoiceData invoiceData;
+
+  InvoiceCreation({
+    required this.enabled,
+    required this.invoiceData,
+  });
+
+  factory InvoiceCreation.fromJson(Map<String, dynamic> json) =>
+      _$InvoiceCreationFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InvoiceCreationToJson(this);
+}
+
+@JsonSerializable()
+class InvoiceData {
+  final dynamic accountTaxIds;
+  final dynamic customFields;
+  final String? description;
+  final String? footer;
+  final String? issuer;
+  final Map<String, String>? metadata;
+  final dynamic renderingOptions;
+
+  InvoiceData({
+    this.accountTaxIds,
+    this.customFields,
+    this.description,
+    this.footer,
+    this.issuer,
+    this.metadata,
+    this.renderingOptions,
+  });
+
+  factory InvoiceData.fromJson(Map<String, dynamic> json) =>
+      _$InvoiceDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InvoiceDataToJson(this);
+}
+
+@JsonSerializable()
+class PhoneNumberCollection {
+  final bool enabled;
+
+  PhoneNumberCollection({
+    required this.enabled,
+  });
+
+  factory PhoneNumberCollection.fromJson(Map<String, dynamic> json) =>
+      _$PhoneNumberCollectionFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PhoneNumberCollectionToJson(this);
+}
+
+@JsonSerializable()
+class TotalDetails {
+  final int amountDiscount;
+  final int amountShipping;
+  final int amountTax;
+
+  TotalDetails({
+    required this.amountDiscount,
+    required this.amountShipping,
+    required this.amountTax,
+  });
+
+  factory TotalDetails.fromJson(Map<String, dynamic> json) =>
+      _$TotalDetailsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TotalDetailsToJson(this);
 }
